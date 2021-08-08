4fc067192eed61784cec77c4a70c597bc3c8f74d
Blessing Krofegha <bkrofegha@gmail.com>
2021-08-08 22:17:37 +0100

    remove bodyparser middleware

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-// require('dotenv/config');
 const app = express();
 
-//Import Routes
-
-
-//Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-//Routes Middleware
 app.use("/", allRoute);
 
 module.exports = app;
